c8adefd2b2234e93af7a0c4c1225d30110a1a944
Chris Aurora Neumann Ruud <66010392+chrisnruud@users.noreply.github.com>
2024-02-13 09:55:11 +0100

    chore: migrate to @stylistic with some default rules override

--- index.js
+++ index.js
@@ -4,25 +4,34 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   plugins: [
-    'vuejs-accessibility'
+    'vuejs-accessibility',
+    '@stylistic'
   ],
   extends: [
+    'plugin:@stylistic/recommended-extends',
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
-    'plugin:vuejs-accessibility/recommended'
+    'plugin:vuejs-accessibility/recommended',
+    'plugin:@stylistic/disable-legacy'
   ],
   env: {
     'vue/setup-compiler-macros': true,
-    node: true,
-    browser: true,
-    es2021: true
+    'node': true,
+    'browser': true,
+    'es2021': true
   },
-  ignorePatterns: ['dist/**'],
+  ignorePatterns: [ 'dist/**' ],
   rules: {
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'never'],
-    indent: ['error', 2],
+    '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
+    '@stylistic/comma-dangle': [ 'error', 'never' ],
+    '@stylistic/indent': [ 'error', 2 ],
+    '@stylistic/object-curly-spacing': [ 'error', 'always' ],
+    '@stylistic/padded-blocks': [ 'error', 'never' ],
+    '@stylistic/quotes': [ 'error', 'single', { avoidEscape: true } ],
+    '@stylistic/semi': [ 'error', 'never' ],
+    '@stylistic/space-before-function-paren': [ 'error', 'always' ],
+    '@stylistic/quote-props': [ 'error', 'consistent-as-needed' ],
     'vue/array-bracket-spacing': 'error',
     'vue/arrow-spacing': 'error',
     'vue/block-spacing': 'error',
@@ -32,12 +41,12 @@ module.exports = {
     'vue/component-name-in-template-casing': 'error',
     'vue/eqeqeq': 'error',
     'vue/key-spacing': 'error',
-    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-empty-component-block': 'error',
+    'vue/object-curly-spacing': [ 'error', 'always' ],
     'vue/space-infix-ops': 'error',
     'vue/space-unary-ops': 'error',
-    'vue/v-on-function-call': 'error',
-    'vue/no-empty-component-block': 'error',
-    'vue/multi-word-component-names': 'off'
+    'vue/v-on-function-call': 'error'
   },
   parserOptions: {
     requireConfigFile: false,
